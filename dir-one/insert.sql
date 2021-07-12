44d8278602d40416a8e4517bde63e0372c870e78
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-12 22:34:48 +0200

    Add two sql statements

---
+++ dir-one/insert.sql
@@ -0,0 +1,3 @@
+insert into content values (1, 'one'  );
+insert into content values (2, 'two'  );
+insert into content values (3, 'three');
---
+++ dir-two/dir with spaces/insert-statements.sql
@@ -0,0 +1,4 @@
+insert into theTable values ('one'  , 7 + 9);
+insert into theTable values ('two'  , 8 + 2);
+insert into theTable values ('three', 4 + 2);
+
