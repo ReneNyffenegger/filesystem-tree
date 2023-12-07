f9b5549d0e2bdcd252193c3697b59ff9f8c75f0e
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-12-07 20:57:01 +0100

    dir-two/sub-dir/sub-sub-dir/sub-sub-sub-dir/sub-sub-sub-sub-dir/sub-sub-sub-sub-file

--- set-permissions.sh
+++ set-permissions.sh
@@ -1,1 +1,7 @@
-chmod 117 dir-two/unreadable-directory
+chmod 111 dir-two/unreadable-directory
+chmod 000 dir-two/unreadable-file.txt
+chmod 777 dir-two/sub-dir
+chmod 775 dir-two/sub-dir/sub-sub-dir
+chmod 755 dir-two/sub-dir/sub-sub-dir/sub-sub-sub-dir
+chmod 555 dir-two/sub-dir/sub-sub-dir/sub-sub-sub-dir/sub-sub-sub-sub-dir
+chmod 444 dir-two/sub-dir/sub-sub-dir/sub-sub-sub-dir/sub-sub-sub-sub-dir/sub-sub-sub-sub-file
